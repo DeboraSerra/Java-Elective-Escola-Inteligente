95016756b35745148e76fbaf0296b4462639e0bb
Débora Serra <debora.r.serra@gmail.com>
2024-05-22 09:19:57 -0300

    feat: add requirement two

--- src/main/java/com/trybe/java/escolainteligente/Secretaria.java
+++ src/main/java/com/trybe/java/escolainteligente/Secretaria.java
@@ -8,14 +8,20 @@ public class Secretaria {
    * Método calcularMedia.
    */
   public static double calcularMedia(double nota1, double nota2, double nota3, double nota4) {
-    System.out.println("TODO: Implementar a função calcular média.");
-    return 0;
+    return (nota1 + nota2 + nota3 + nota4) / 4;
   }
 
   /**
    * Método coletarInformacoes.
    */
   public static void coletarInformacoes() {
-    System.out.println("TODO: Solicitar informações para cálculo da média ");
+    Scanner scanner = new Scanner(System.in);
+    String name = scanner.next();
+    double grade1 = Double.parseDouble(scanner.next());
+    double grade2 = Double.parseDouble(scanner.next());
+    double grade3 = Double.parseDouble(scanner.next());
+    double grade4 = Double.parseDouble(scanner.next());
+    double average = Secretaria.calcularMedia(grade1, grade2, grade3, grade4);
+    System.out.println("A média das notas de " + name + " é " + average);
   }
 }
